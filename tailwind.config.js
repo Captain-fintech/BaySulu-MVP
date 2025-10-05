3d08fbe2b08a52dd33f369d4ed184b5215f50449
Captain-fintech <y.akayev@gmail.com>
2025-10-05 04:35:02 +0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** tailwind.config.js */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#16543C"  /* фирменный зелёный */
+      }
+    }
+  },
+  plugins: []
+};
